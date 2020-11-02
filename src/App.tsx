c25992acdd3c6329e59246ca123b585197db52de
faiza203 <fiazfaiza981@gmail.com>
2020-11-02 16:31:15 +0500

    Update HTML elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import {QuestionCard} from "./components/QuestionCard"
 import './App.css';
 
 function App() {
+  const startTrivia = ( e : React.MouseEvent<HTMLButtonElement>) => {
+}
+const nextQuestion = () => {
+
+}
   return (
     <div className="App">
-      I am react
+      <h1>I am react</h1>
+      <button className="start" onClick={startTrivia}>
+        Start
+      </button>
+      <p className="score">Score</p>
+      <p>Loading Questions...</p>
+      <QuestionCard />
+      <button className="next" onClick={nextQuestion}>
+        Next
+      </button>
     </div>
   );
 }
