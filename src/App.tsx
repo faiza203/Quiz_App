381e94be2796e22177cddcad0e620c198dd3fed5
faiza203 <fiazfaiza981@gmail.com>
2020-11-04 17:07:09 +0500

    Create Card

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import React, {useState} from 'react';
-import {QuestionState , Difficulty} from "./components/DifficultyType";
-import {fetchQuizQuestions} from "./API";
-import {AnswerObject} from "./components/AnswerType";
+import React, { useState } from 'react';
+import { QuestionState, Difficulty , AnswerObject , QuestionCard} from "./components/index";
+import { fetchQuizQuestions } from "./API";
 import './App.css';
 
 function App() {
   const total_Questions = 10;
-  const [loading , setLoading] = useState(false);
-  const [questions , setQuestion] = useState<QuestionState[]>([]);
-  const [number , setNumber] = useState(0);
-  const [userAnswers , setUserAnswers] = useState<AnswerObject[]>([]);
-  const [score , setScore] = useState(0);
-  const [gameOver , setGameOver] = useState(true);
-  console.log(fetchQuizQuestions(total_Questions , Difficulty.EASY)); 
-   const startTrivia = async () => {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestion] = useState<QuestionState[]>([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+  console.log(fetchQuizQuestions(total_Questions, Difficulty.EASY));
+  const startTrivia = async () => {
     setLoading(true);
     setGameOver(false);
     const newQuestions = await fetchQuizQuestions(
@@ -25,7 +24,7 @@ function App() {
     setUserAnswers([]);
     setNumber(0);
     setLoading(false);
-   };
+  };
   const checkAnwer = (e: React.MouseEvent<HTMLButtonElement>) => {
   }
   const nextQuestion = () => {
@@ -34,18 +33,23 @@ function App() {
   return (
     <div className="App">
       <h1>I am react</h1>
-      <button className="start" onClick={startTrivia}>
+      {gameOver || userAnswers.length === total_Questions ? (<button className="start" onClick={startTrivia}>
         Start
       </button>
-      <p className="score">Score</p>
-      <p>Loading Questions...</p>
-      {/* <QuestionCard 
+      ) : null
+      }
+      {!gameOver ? <p className="score">score</p> : null}
+      {loading && <p>Loading Questions...</p>}
+    {!loading && !gameOver && (
+      <QuestionCard 
       questionNr={number + 1} totalQuestions={total_Questions}
       question={questions[number].question}
       answers={questions[number].answer}
       userAnswer={userAnswers ? userAnswers[number] : undefined}
       callback={checkAnwer}
-      /> */}
+      />
+    )}
+
       <button className="next" onClick={nextQuestion}>
         Next
       </button>
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,6 @@
-export type {AnswerObject} from "./AnswerType";
-export type {Question} from "./QuestionType"
-export {Difficulty} from "./DifficultyType";
-export type {QuestionState} from "./DifficultyType";
-export type {Props} from "./PropsType";
+export type { AnswerObject } from "./AnswerType";
+export type { Question } from "./QuestionType";
+export { Difficulty } from "./DifficultyType";
+export { QuestionCard } from "./QuestionCard";
+export type { QuestionState } from "./DifficultyType";
+export type { Props } from "./PropsType";
