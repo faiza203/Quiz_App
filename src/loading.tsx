fd7a53538820b2defec66ad12da631b08f592db3
faiza203 <fiazfaiza981@gmail.com>
2020-11-06 15:09:41 +0500

    Create Loading Div

---
+++ src/loading.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+export const loading = () => {
+<div id="loading">
+<div className="spinner-border" role="status">
+    <span className="sr-only">Loading...</span>
+</div>
+</div>
+};
